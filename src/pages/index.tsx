933ac511e4f7390a9aabe1d039c52177553b46f9
lehach <lehach@x-cart.com>
2023-10-01 13:38:40 +0400

    Removing unused public folder, removing Suspense (because lazy is not used for now)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,13 @@ import {RatesChartPage} from "./RatesChartPage";
 
 export const Router: FC = () => (
     <BrowserRouter>
-        <Suspense fallback="Загрузка...">
+        {/*<Suspense fallback="Загрузка...">*/}
             <Routes>
                 <Route path={ROUTE_CONSTANTS.HOME} element={<HomePage/>}/>
                 <Route path={ROUTE_CONSTANTS.CONVERTER} element={<ConverterPage/>}/>
                 <Route path={ROUTE_CONSTANTS.RATES_CHART} element={<RatesChartPage/>}/>
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
-        </Suspense>
+        {/*</Suspense>*/}
     </BrowserRouter>
 );
