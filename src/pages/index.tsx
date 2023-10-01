a92f7c4cb681c209b090cd876d1af45ce75e4c8d
lehach <lehach@x-cart.com>
2023-10-01 13:39:34 +0400

    Removing import Suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {FC, Suspense} from "react";
+import {FC} from "react";
 
 import {ROUTE_CONSTANTS} from "@/shared/config";
 import {Route, Routes, BrowserRouter, Navigate} from "react-router-dom";
@@ -8,13 +8,11 @@ import {RatesChartPage} from "./RatesChartPage";
 
 export const Router: FC = () => (
     <BrowserRouter>
-        {/*<Suspense fallback="Загрузка...">*/}
-            <Routes>
-                <Route path={ROUTE_CONSTANTS.HOME} element={<HomePage/>}/>
-                <Route path={ROUTE_CONSTANTS.CONVERTER} element={<ConverterPage/>}/>
-                <Route path={ROUTE_CONSTANTS.RATES_CHART} element={<RatesChartPage/>}/>
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        {/*</Suspense>*/}
+        <Routes>
+            <Route path={ROUTE_CONSTANTS.HOME} element={<HomePage/>}/>
+            <Route path={ROUTE_CONSTANTS.CONVERTER} element={<ConverterPage/>}/>
+            <Route path={ROUTE_CONSTANTS.RATES_CHART} element={<RatesChartPage/>}/>
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
     </BrowserRouter>
 );
