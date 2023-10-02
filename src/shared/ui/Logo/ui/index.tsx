8f66f955b3fe29e76212adfcc72d731fb8f40ff9
lehach <lehach@x-cart.com>
2023-10-02 09:56:04 +0400

    Adding link to the app logo

--- src/shared/ui/Logo/ui/index.tsx
+++ src/shared/ui/Logo/ui/index.tsx
@@ -1,8 +1,14 @@
 import './styles.css';
 import LogoImage from '@/shared/assets/logo.svg';
+import { NavLink } from "react-router-dom";
 
 export const Logo = () => {
     return (
-        <img className="logo" src={LogoImage}  />
+        <NavLink
+            to="/"
+            className="logo-link"
+        >
+            <img className="logo" src={LogoImage}  />
+        </NavLink>
     );
 };
